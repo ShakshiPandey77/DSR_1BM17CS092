84d659f2f4eae13d17cc8f7da392259eef1b5af1
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2020-10-13 13:23:10 +0530

    Add files via upload

---
+++ Lab5/Lab5_program.R
@@ -0,0 +1,11 @@
+"
+Export a vector to 3 txt files.
+"
+data <- c(23.0,17.0,12.5,11.0,17.0,12.0,14.5,9.0,11.0,9.0,12.5,14.5,17.0)
+data
+length(data)
+write(data, file = "Desktop/Lab5/data1.txt",sep = "\t")
+# creating a columnwise output based on length
+write(data, file = "Desktop/Lab5/data2.txt",sep=",",ncolumns = length(data))
+# creating a single column
+write(data, file = "Desktop/Lab5/data3.txt", sep = ",", ncolumns = 1)
