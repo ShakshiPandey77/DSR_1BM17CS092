8b36eca4c8c4e3ce34434cb315bfd6dc8d11bdc2
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2020-10-29 22:07:45 +0530

    Update and rename Lab_10(1BM17CS092).R to Lab_8(1BM17CS092).R

--- Lab8/Lab_8(1BM17CS092).R
+++ Lab8/Lab_8(1BM17CS092).R
@@ -2,7 +2,7 @@ install.packages('RODBC')
 
 library('RODBC')
 
-conn<-odbcConnect("MYSQL","root","rounak")
+conn<-odbcConnect("MYSQL","root","shakshi")
 
 employee<-sqlQuery(conn,"select emp_no,first_name,last_name,gender from employees_mod.t_employees;")
 employee
